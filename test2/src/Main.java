c84096306ac38fe3028263fb4e2bce2b95905d5a
ShockwaveZA <minal.pramlall@yahoo.com>
2017-07-24 12:46:06 +0200

    Update Main.java

--- test2/src/Main.java
+++ test2/src/Main.java
@@ -1,9 +1,11 @@
+import java.io.*;
+
 public class Main {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         TestBenchmark t1 = new TestBenchmark(0);
         t1.start();
         
@@ -13,11 +15,45 @@ public class Main {
         TestBenchmark t3 = new TestBenchmark(2);
         t3.start();
         
-        String json = "{";
-        json += t1.toJsonArray() + ",";
-        json += t2.toJsonArray() + ",";
-        json += t3.toJsonArray() + "}";
+        // -------------------- Export Results
         
-        System.out.println("\n" + json);
+        // ---------- Runtime
+        
+        String data = "\"chart\":{\"caption\":\"Runtime\",\"theme\":\"ocean\"},\"data\":[
+        data += t1.getBenchmark().getRuntime() + ",";
+        data += t2.getBenchmark().getRuntime() + ",";
+        data += t3.getBenchmark().getRuntime() + "]}";
+        
+        FileWriter f = new FileWriter("runtime.json", false);
+        PrintWriter p = new PrintWriter(f);
+        
+        p.print(data);
+        p.close();
+        
+        // ---------- Memory Usage
+        
+        String data = "\"chart\":{\"caption\":\"Memory Usage\",\"theme\":\"ocean\"},\"data\":[
+        data += t1.getBenchmark().getMemoryUsage() + ",";
+        data += t2.getBenchmark().getMemoryUsage() + ",";
+        data += t3.getBenchmark().getMemoryUsage() + "]}";
+        
+        FileWriter f = new FileWriter("memoryUsage.json", false);
+        PrintWriter p = new PrintWriter(f);
+        
+        p.print(data);
+        p.close();
+        
+        // ---------- CPU Load
+        
+        String data = "\"chart\":{\"caption\":\"CPU Load\",\"theme\":\"ocean\"},\"data\":[
+        data += t1.getBenchmark().getCPULoad() + ",";
+        data += t2.getBenchmark().getCPULoad() + ",";
+        data += t3.getBenchmark().getCPULoad() + "]}";
+        
+        FileWriter f = new FileWriter("CPULoad.json", false);
+        PrintWriter p = new PrintWriter(f);
+        
+        p.print(data);
+        p.close();
     }
 }
