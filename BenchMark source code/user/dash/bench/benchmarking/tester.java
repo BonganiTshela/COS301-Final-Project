2674d508329306164b89800a72ce6073099450aa
Joseph Letsoalo <josephletsoalo196@gmail.com>
2017-08-09 20:01:29 +0200

    Add files via upload

---
+++ BenchMark source code/user/dash/bench/benchmarking/tester.java
@@ -0,0 +1,7 @@
+public class tester {
+    public static void tester()
+    {
+       int arr[] = {12,9,4,99,120,1,3,10,12,150,35,186,5,423,20,10,15,35,89,45,101};
+       CombSort.sort(arr); 
+    }
+}
