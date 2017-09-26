40765fc321e9321e17a4cfd4edceff8caa4de202
ShockwaveZA <minal.pramlall@yahoo.com>
2017-09-26 02:54:59 +0200

    Add files via upload

---
+++ finalBenchmarker/uploaded/fibonacci.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class fibonacci {
+
+	public static void main(String[] args) {
+		String name = args[0];
+		try {
+			InputStream is = new FileInputStream(name);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			int n = Integer.parseInt(br.readLine());
+	
+			if (n <= 2)
+				return;
+			long a = 1, b = 1, c;
+			for (int k = 0; k < n - 2; ++k) {
+				c = a + b;
+				b = a;
+				a = c;
+			}
+		} catch (Exception e) {}
+	}
+}
