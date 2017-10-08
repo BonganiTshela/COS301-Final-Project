7e76042e55e1b48345aac0fb96526abdd3347836
ShockwaveZA <minal.pramlall@yahoo.com>
2017-10-08 20:40:20 +0200

    Update s1.sh

--- finalBenchmarker/s1.sh
+++ finalBenchmarker/s1.sh
@@ -1,3 +1,6 @@
+rm benchmarking/json/*
+rm benchmarking/testFiles/*
+cp -rf benchmarking/original/* benchmarking
 cp uploaded/*.txt benchmarking/testFiles
 ALGOS="$(java Primer)"
 javac benchmarking/*.java
