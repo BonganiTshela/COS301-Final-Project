027e840dd288e27867d5b51718b366205883ea5d
Minal Pramlall <minal.pramlall@yahoo.com>
2017-10-11 23:54:01 +0300

    Delete tester2.java

--- BenchMark source code/user/dash/bench/benchmarking/tester2.java
+++
@@ -1,7 +0,0 @@
-  public class tester2 {
-    public static void tester2()
-    {
-        int arr[] = {12,9,4,99,120,1,3,10,12,150,35,186,5,423,20,10,15,35,89,45,101};
-        MergeSort.sort(arr, 0, arr.length-1);
-    }
-}
