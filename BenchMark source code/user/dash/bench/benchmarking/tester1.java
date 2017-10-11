bff4a2ba64989d6f6bb610b0b3d13ad19e637bc5
Minal Pramlall <minal.pramlall@yahoo.com>
2017-10-11 23:56:36 +0300

    Delete tester1.java

--- BenchMark source code/user/dash/bench/benchmarking/tester1.java
+++
@@ -1,7 +0,0 @@
-
-public class tester1 {
-    public static void tester1(){
-        int array[] = {12,9,4,99,120,1,3,10,12,150,35,186,5,423,20,10,15,35,89,45,101};
-        selection.selection( array, array.length);
-    }
-}
