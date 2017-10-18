07a003804e64b882847755274bc954832bb43db4
ProBlack95 <u14134790@tuks.co.za>
2017-10-18 13:40:40 +0200

    Add files via upload

--- finalBenchmarker/s1.sh
+++ finalBenchmarker/s1.sh
@@ -4,5 +4,6 @@ cp -rf benchmarking/original/* benchmarking
 cp uploaded/*.txt benchmarking/testFiles
 ALGOS="$(java Primer)"
 javac benchmarking/*.java
+rm uploaded/*
 cd benchmarking
 java Main "$ALGOS"
